a5cd9c8f43155082f31150086de31056c78b984c
avan-dam <ambervandam97@gmail.com>
2021-07-01 11:42:07 +0200

    added main for testing

--- main.c
+++ main.c
@@ -3,15 +3,11 @@
 
 int	main(void)
 {
-    char    a;
-    char    b[6];
-    int     c;
-    int     d;
+    char    a = 'a';
+    char    b[6] = "hello";
+    int     c = 9393993;
+    int     d = 384784874;
 
-    a = 'a';
-    b[6] = "hello";
-    c = 9393993;
-    d = 384784874;
     printf("output from printf is:\n[%c], [%.7s], [%7.s], [%077d], [%x]\n", a, b, b, c, d);
     ft_printf("output from my (ft_)printf is:\n[%c], [%.7s], [%7.s], [%077d], [%x]\n", a, b, b, c, d);
 }
